573f3db7d37e5ed491ac678f8130230a73f1fac0
davidmizra17 <david.mizrahi@correo.unimet.edu.ve>
2023-08-22 15:29:45 -0400

    Frontend Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import { ChakraProvider } from '@chakra-ui/react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <p>ChatEz</p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
