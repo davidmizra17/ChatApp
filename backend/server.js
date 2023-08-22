16a13d9354d5bece8a594354405dcf98382c0d36
davidmizra17 <david.mizrahi@correo.unimet.edu.ve>
2023-08-22 10:03:23 -0400

    dummy data and API implementation

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
 
 const app = express();
+dotenv.config()
 
 app.get('/', (req, res) => {
-    res.send("API is running");
+    res.send("API is running Succesfully");
 });
 
+app.get('/api/chat', (req, res) => {
+    res.send(chats)
+})
 
 
-app.listen(8000, console.log("started server on PORT 8000"));
+app.get('/api/chat/:id', (req, res) => {
+    // console.log(req.params.id)
+    const singleChat = chats.find(c => c._id === req.params.id)
+    res.send(singleChat)
+})
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, console.log(`started server on PORT ${PORT}`));
