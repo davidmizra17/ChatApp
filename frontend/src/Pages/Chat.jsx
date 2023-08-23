e120547280a752ae674cf566f9599877a2fc7066
davidmizra17 <david.mizrahi@correo.unimet.edu.ve>
2023-08-23 10:36:13 -0400

    Models creation and some styling

--- frontend/src/Pages/Chat.jsx
+++ frontend/src/Pages/Chat.jsx
@@ -1,18 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from "axios"
 
 const Chat = () => {
 
+    const [chats, setChats] = useState([]);
+
     const fetchChats = async () => {
         const { data } = await axios.get('http://127.0.0.1:8000/api/chat')
-        console.log(data)
+        setChats(data)
     }
 
     useEffect(() => {
         fetchChats();
     }, [])
   return (
-    <div>Chat</div>
+      <div>{chats.map(chat => (
+          <div key={chat._id}>{chat.chatName}</div>
+      ))}</div>
   )
 }
 
