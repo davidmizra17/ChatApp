b34f79161daa651b0de45b70bb6fa183fa8e8b5a
davidmizra17 <david.mizrahi@correo.unimet.edu.ve>
2023-08-25 22:35:11 -0400

    Group Chat API implementations

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
-const { accessChat } = require("../controllers/chatsControllers")
+const {
+    accessChat,
+    fetchChats,
+    createGroupChat,
+    renameGroup,
+    addToGroup,
+    removeFromGroup
+} = require("../controllers/chatsControllers")
+
 
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
-// router.route("/").get(protect, fetchChats)
+router.route("/").get(protect, fetchChats)
 
-// router.route("/group").post(protect, createGroupChat);
-// router.route("/rename").put(protect, renameGroup)
-// router.route("/groupremove").put(protect, removeFromGroup);
-// router.route("/addmember").put(protect, addToGroup);
+router.route("/group").post(protect, createGroupChat);
+router.route("/rename").put(protect, renameGroup)
+router.route("/addmember").put(protect, addToGroup);
+router.route("/remove").put(protect, removeFromGroup);
 
 module.exports = router;
